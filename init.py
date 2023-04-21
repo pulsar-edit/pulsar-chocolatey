80c1dcd40d920ab3e0b3ed7158ef1d7bc57c7c02
confused-Techie <dev@lhbasics.com>
2023-04-20 19:41:19 -0700

    Attempted to create and publish version `1.104.0`

--- init.py
+++ init.py
@@ -15,52 +15,52 @@ class PackageFilesEditor():
 	INSTALL_SCRIPT_PATH = PACKAGE_SOURCES_PATH + "tools/chocolateyinstall.ps1"
 	INSTALLER_PATH = PACKAGE_SOURCES_PATH + "tools/"
 	VERIFICATION_FILE_PATH = INSTALLER_PATH + "VERIFICATION.txt"
-	
+
 	# Class properties
 	pulsarVersion_ = 0
 	installerUrl_ = "https://github.com/pulsar-edit/pulsar/releases/download/"
 	installerFileName_ = ""
 	installerChecksum_ = ""
-	
+
 	# Class initializer
 	def __init__(self, pulsarVersion):
 		self.pulsarVersion_ = pulsarVersion
 		self.installerFileName_ = "Windows.Pulsar.Setup." + pulsarVersion + ".exe"
 		self.installerUrl_ = self.installerUrl_ + "v" + pulsarVersion + "/" + self.installerFileName_
-	
+
 	# Main function run from main
 	def run(self):
 		print("Init configuration files to generate package for version " + self.pulsarVersion_)
-		
+
 		self.downloadInstaller()
-		
+
 		self.editNuspecPackage()
 		self.editInstallScript()
 		self.generateVerificationFile()
-		
+
 		print("Package sources initialized")
-		
-	
+
+
 	def downloadInstaller(self):
 		print("Download installer for version " + self.pulsarVersion_ + "...")
-		
+
 		response = requests.get(self.installerUrl_)
 		if(response.status_code != 200):
 			print("Error " + str(response.status_code) + " while downloading install file")
 			sys.exit(1)
-		
+
 		open(self.INSTALLER_PATH + self.installerFileName_, "wb").write(response.content)
-		
+
 		print("DONE")
-		
+
 		print("Compute SHA256 checksum...")
-		
+
 		with open(self.INSTALLER_PATH + self.installerFileName_, "rb") as f:
 			bytes = f.read() # read entire file as bytes
 			self.installerChecksum_ = hashlib.sha256(bytes).hexdigest();
-			
+
 		print("DONE")
-	
+
 	def editNuspecPackage(self):
 		print("Edit " + self.NUSPEC_FILE + "file...")
 		nuspecFileOriginal = codecs.open(self.NUSPEC_FILE, "r", "utf-8")
@@ -68,16 +68,18 @@ class PackageFilesEditor():
 		for line in nuspecFileOriginal:
 			if "<version>" in line:
 				nuspecFileNew.write("    <version>" + self.pulsarVersion_ + "</version>" + os.linesep)
+			if "<releaseNotes>" in line:
+				nuspecFileNew.write("	<releaseNotes>https://github.com/pulsar-edit/pulsar/blob/master/CHANGELOG.md#" + re.sub(r'[^\w\s]', '', self.pulsarVersion_) + "</releaseNotes>" + os.linesep)
 			else:
 				nuspecFileNew.write(line)
 		nuspecFileOriginal.close()
 		nuspecFileNew.close()
-		
+
 		shutil.copy2("tmp", self.NUSPEC_FILE)
 		os.remove("tmp")
-		
+
 		print("DONE")
-		
+
 	def editInstallScript(self):
 		print("Edit " + self.INSTALL_SCRIPT_PATH + "file...")
 		installScriptOriginal = codecs.open(self.INSTALL_SCRIPT_PATH, "r", "utf-8")
@@ -91,37 +93,36 @@ class PackageFilesEditor():
 				installScriptNew.write(line)
 		installScriptOriginal.close()
 		installScriptNew.close()
-		
+
 		shutil.copy2("tmp", self.INSTALL_SCRIPT_PATH)
 		os.remove("tmp")
-		
+
 		print("DONE")
-		
+
 	def generateVerificationFile(self):
 		print("Generate verification file...");
 		verificationFile = codecs.open(self.VERIFICATION_FILE_PATH, "w", "utf-8")
-		
+
 		verificationFile.write("VERIFICATION" + os.linesep)
 		verificationFile.write(os.linesep)
 		verificationFile.write("Installer file: " + self.installerFileName_ + os.linesep)
 		verificationFile.write("Checksum: " + self.installerChecksum_ + os.linesep)
 		verificationFile.write("Checksum type: SHA256" + os.linesep)
 		verificationFile.write("Original installer and checksums can be found at: https://github.com/pulsar-edit/pulsar/releases/tag/v" + self.pulsarVersion_ + os.linesep)
-		
+
 		verificationFile.close()
-		
+
 		print("DONE")
-	
+
 # Main function
 if __name__ == '__main__':
 	# Command line arguments parsing
 	parser = argparse.ArgumentParser(description="Init Chocolatey Pulsar package configuration files", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument("version", help="Pulsar version")
 	args = parser.parse_args()
-	
+
 	# Run files edit
 	packageFilesEditor = PackageFilesEditor(args.version)
 	packageFilesEditor.run()
-	
+
 	print("Navigate to pulsar folder and generate package with 'choco pack' command")
-		
--- pulsar/tools/chocolateyinstall.ps1
+++ pulsar/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'Windows.Pulsar.Setup.1.103.0.exe'
+$fileLocation = Join-Path $toolsDir 'Windows.Pulsar.Setup.1.104.0.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -9,7 +9,7 @@ $packageArgs = @{
   fileType       = 'exe'
   file           = $fileLocation
   softwareName   = 'Pulsar'
-  checksum       = 'e63c7c33c1d98762331ce3964eaf208bd868ae20f0642d119fe9e257eafb9e72'
+  checksum       = '253449889bdeb5c6b48ed7deb8e6b4e853da1624b06fe2a53c0663e508392ab7'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
