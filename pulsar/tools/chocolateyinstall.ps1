d6392d6828eaccab020a2822c719c51d2d571293
confused-Techie <confused-Techie@users.noreply.github.com>
2023-08-20 20:38:18 +0000

    Draft 1.107.0 Release

--- pulsar/tools/chocolateyinstall.ps1
+++ pulsar/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url = 'https://github.com/pulsar-edit/pulsar/releases/download/v1.106.0/Windows.Pulsar.Setup.1.106.0.exe'
+$url = 'https://github.com/pulsar-edit/pulsar/releases/download/v1.107.0/Windows.Pulsar.Setup.1.107.0.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -7,7 +7,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = $url
   softwareName   = 'Pulsar'
-  checksum       = 'a1db7bb0aed97607136a54ae3e147e09103f27bed4d2dea4ec384c63a2fc632b'
+  checksum       = '1b16445e9614bd0d2603b08d9b3e038bad1ae0ab3ffa297011372e6f40b53784'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
